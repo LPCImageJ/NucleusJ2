73089a1aa1bd8d54e7f8dce2a22a6dd95327ab02
Tristan Dubos <tristan.duos33@gmail.com>
2018-12-04 19:04:00 +0100

    Output GIFT

--- src/main/java/gred/nucleus/test/TestSegmentationMethods.java
+++ src/main/java/gred/nucleus/test/TestSegmentationMethods.java
@@ -55,7 +55,8 @@ public class TestSegmentationMethods {
         String imgPathTristan = "";
         String imgSegPathTristan = "";
         String inputTristan = "/home/tridubos/Bureau/180516_FIXE_Col_J13_H258/COL0_BAD_SEG/TEST";
-        String outputTristan = "/home/tridubos/Bureau/180516_FIXE_Col_J13_H258/COL0_BAD_SEG/GIFT";
+        String outputTristanGift = "/home/tridubos/Bureau/180516_FIXE_Col_J13_H258/COL0_BAD_SEG/GIFT";
+        String outputTristanOtsu = "/home/tridubos/Bureau/180516_FIXE_Col_J13_H258/COL0_BAD_SEG/OTSU";
         ImagePlus img  = IJ.openImage(imgPathAxel);
 
         //testStupid(img,(short)6.0, (short)40.0,imgSegPathAxel,true);
@@ -64,7 +65,9 @@ public class TestSegmentationMethods {
         cal.pixelDepth = 0.2;
         cal.pixelHeight = 0.103;
         cal.pixelWidth = 0.103;
-        testStupidSeveralImages(inputTristan, outputTristan, (short)6.0, (short)300.0,cal,true);
+        testStupidSeveralImages(inputTristan, outputTristanGift, (short)6.0, (short)300.0,cal,true);
+        testStupidSeveralImages(inputTristan, outputTristanOtsu, (short)6.0, (short)300.0,cal,false);
+
 
         System.err.println("The program ended normally.");
     }
