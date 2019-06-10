86146d488effd9494dfc2d7c4b1098f84dc5c0ab
Tristan Dubos <tristan.duos33@gmail.com>
2019-06-10 16:38:11 +0200

    Add comment class Directory

--- src/main/java/gred/nucleus/FilesInputOutput/Directory.java
+++ src/main/java/gred/nucleus/FilesInputOutput/Directory.java
@@ -6,7 +6,7 @@ import java.lang.*;
 import java.util.ArrayList;
 
 /**
- * Class get to get Directory information.
+ * Class get to list directory and sub directory.
  *
  *
  */
@@ -17,22 +17,33 @@ public class Directory {
      * Directory path
      */
     public String _dirPath = "";
+    /** List of files in current forlder + recursive folder */
     public ArrayList<File> m_listeOfFiles = new ArrayList<File>();
+    /** Check if directory contain nd files */
     public Boolean m_containNdFile=false;
+    /** List of nd files */
     public ArrayList<File> m_listeOfFilesND = new ArrayList<File>();
 
-
+    /** Constructor
+     * @param Path of directory
+     */
     public Directory(String Path) {
         this._dirPath = Path;
 
     }
 
+    /**
+     * Method to check if directory and create if doesn't
+     */
     public void CheckAndCreateDir() {
         ChekSeparatorEndPath();
         CreateDire();
 
     }
 
+    /**
+     * Check if separator exist
+     */
     private void ChekSeparatorEndPath() {
         File dir = new File(this._dirPath);
         Character SeparatorEnd = this._dirPath.charAt(this._dirPath.length() - 1);
@@ -43,6 +54,9 @@ public class Directory {
 
     }
 
+    /**
+     * Method creating folder if doesn't exist.
+     */
     private void CreateDire() {
         File dir = new File(this._dirPath);
         if (!dir.exists()) {
@@ -50,16 +64,24 @@ public class Directory {
         }
     }
 
+    /**
+     *
+     * @return path current directory
+     */
     public String get_dirPath() {
         return this._dirPath;
     }
 
-    public void GetListFiles(String Path) {
+    /**
+     * Method to recursively list files contains in folder and subfolder. (Argument needed because of recursive way)
+     * @param Path path of folder
+     */
+    public void listFiles(String Path) {
         File root = new File(Path);
         File[] list = root.listFiles();
         for (File f : list) {
             if (f.isDirectory()) {
-                GetListFiles(f.getAbsolutePath());
+               listFiles(f.getAbsolutePath());
             } else {
                 this.m_listeOfFiles.add(f);
                 if(FilenameUtils.getExtension(f.getName()).equals("nd")) {
@@ -71,31 +93,42 @@ public class Directory {
         }
     }
 
+    /**
+     * Replace list files if ND files have been listed.
+     */
+    public void checkAndActualiseNDFiles(){
+        if(this.m_containNdFile ==true){
+            this.m_listeOfFiles=this.m_listeOfFilesND;
+        }
+    }
+
+    /**
+     * @return list of files
+     */
+
     public ArrayList<File> ListFiles() {
         return this.m_listeOfFiles;
     }
 
-
+    /**
+     * @param indice of file in list array
+     * @return File
+     */
     public File getFile(int indice) {
         return this.m_listeOfFiles.get(indice);
     }
+
+    /**
+     *
+     * @return number of file listed
+     */
+
     public int getNumberFiles(){
         return this.m_listeOfFiles.size();
     }
 
 
 
-    public boolean getContainNdFile(){
-        return this.m_containNdFile;
-    }
-    public int getNumberNDFiles(){
-        return this.m_listeOfFilesND.size();
-    }
-    public ArrayList<File> ListFilesND() {
-        return this.m_listeOfFilesND;
-    }
-    public File getNDFile(int indice) {
-        return this.m_listeOfFilesND.get(indice);
-    }
+
 
 }
