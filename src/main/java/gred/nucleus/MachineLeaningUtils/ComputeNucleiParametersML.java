84a8fb6c1f4ddcf3cc3d25d23486af5fd007acf0
Tristan Dubos <tristan.duos33@gmail.com>
2020-05-25 17:15:39 +0200

    Result column name update

--- src/main/java/gred/nucleus/MachineLeaningUtils/ComputeNucleiParametersML.java
+++ src/main/java/gred/nucleus/MachineLeaningUtils/ComputeNucleiParametersML.java
@@ -69,8 +69,26 @@ public class ComputeNucleiParametersML {
      *
      * @return columns names for results
      */
-    public static String getColnameResult(){
-        return "NucleusFileName\tVolume\tFlatness\tElongation\tSphericity\tEsr\tSurfaceArea\tSurfaceAreaCorrected\tSphericityCorrected\tMeanIntensity\tStandardDeviation\tMinIntensity\tMaxIntensity\n";
+    public String getColnameResult(){
+        return "NucleusFileName\t" +
+                "Volume\t" +
+                "Flatness\t" +
+                "Elongation\t" +
+                "Sphericity\t" +
+                "Esr\t" +
+                "SurfaceArea\t" +
+                "SurfaceAreaCorrected\t" +
+                "SphericityCorrected\t" +
+                "MeanIntensityNucleus\t" +
+                "MeanIntensityBackground\t" +
+                "StandardDeviation\t" +
+                "MinIntensity\t" +
+                "MaxIntensity\t" +
+                "MedianIntensityImage\t" +
+                "MedianIntensityNucleus\t" +
+                "MedianIntensityBackground\t" +
+                "ImageSize\t" +
+                "OTSUThreshold\n";
     }
 
     /**
