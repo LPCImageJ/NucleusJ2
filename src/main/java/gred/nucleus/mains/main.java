621f58a60438f27a0b1764a395b32be24037aa14
Tristan Dubos <tristan.duos33@gmail.com>
2020-07-10 09:27:25 +0200

    Add projection from coordinate , and overlay method

--- src/main/java/gred/nucleus/mains/main.java
+++ src/main/java/gred/nucleus/mains/main.java
@@ -1,29 +1,20 @@
 package gred.nucleus.mains;
-import gred.nucleus.FilesInputOutput.Directory;
-import gred.nucleus.FilesInputOutput.OutputTexteFile;
 import gred.nucleus.MachineLeaningUtils.SliceToStack;
 import gred.nucleus.MachineLeaningUtils.ComputeNucleiParametersML;
 import gred.nucleus.autocrop.*;
 import gred.nucleus.core.ComputeNucleiParameters;
-import gred.nucleus.core.Measure3D;
 import gred.nucleus.exceptions.fileInOut;
-import gred.nucleus.plugins.PluginParameters;
 import gred.nucleus.segmentation.SegmentationCalling;
-
 import gred.nucleus.segmentation.SegmentationParameters;
 import ij.ImagePlus;
 import ij.io.FileSaver;
 import loci.common.DebugTools;
 import loci.formats.FormatException;
-import loci.plugins.BF;
-
-import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
+
 
 
 public class main {
-
 
 
     /**
@@ -185,19 +176,28 @@ public class main {
 
 
 
+    // UN DOSSIER AVEC LES IMAGETTES
+    // UN DOSSIER AVEC LES ZPROJECTION
+    // UN DOSSIER AVEC LES COORDONNEES
+
+
+    public static void generateProjectionFromCoordinnates(String pathToCoordonnate, String pathToRaw) throws IOException, FormatException,Exception {
+        generateProjectionFromCoordonne projection =new generateProjectionFromCoordonne(pathToCoordonnate, pathToRaw);
+        System.out.println("le run 2 :: ");
+        projection.run2();
+    }
 
 
 
     // UN DOSSIER AVEC LES IMAGETTES
-    // UN DOSSIER AVEC LES COORDONNEES
     // UN DOSSIER AVEC LES ZPROJECTION
+    // UN DOSSIER AVEC LES COORDONNEES
+
 
     public static void generateProjectionFromCoordinnates(String pathToGIFTSeg, String pathToZprojection,String pathToCoordonnate) throws IOException, FormatException,Exception {
         generateProjectionFromCoordonne projection =new generateProjectionFromCoordonne(pathToGIFTSeg, pathToZprojection, pathToCoordonnate);
         projection.run();
     }
-
-
 
 
     public static void sliceToStack(String pathToSliceDir, String pathToOutputDir) throws Exception {
@@ -206,20 +206,26 @@ public class main {
 
 
     }
-    public static void cropFromCoordinates(String coordonnateDir) throws IOException, FormatException,Exception {
+    public static void cropFromCoordinates(String coordonnateDir) throws Exception {
 
         CropFromCoordonnate test = new CropFromCoordonnate(coordonnateDir);
         test.runCropFromCoordonnate();
     }
+    // DIC_path zprojection_path
+    public static void genereOV(String linkOverlayProjection) throws Exception {
 
+        GenerateOverlay ov = new GenerateOverlay(linkOverlayProjection);
+        ov.run();
+
+    }
     public static void saveFile ( ImagePlus imagePlusInput, String pathFile) {
         FileSaver fileSaver = new FileSaver(imagePlusInput);
         fileSaver.saveAsTiff(pathFile);
     }
 
-    public static void main(String[] args) throws IOException, FormatException, fileInOut,Exception {
+    public static void main(String[] args) throws Exception {
+        // SET OFF BIOFORMATS WARNINGS
         DebugTools.enableLogging("OFF");
-
         System.setProperty("java.awt.headless", "false");
 
         if(args[0].equals("autocrop")) {
@@ -262,13 +268,21 @@ public class main {
             computeNucleusParametersDL(args[1], args[2]);
         }
         else if(args[0].equals("generateProjection")){
-            generateProjectionFromCoordinnates(args[1], args[2],args[3]);
+            if(args.length==4) {
+                generateProjectionFromCoordinnates(args[1], args[2], args[3]);
+            }
+            else{
+                generateProjectionFromCoordinnates(args[1], args[2]);
+            }
         }
         else if (args[0].equals("SliceToStack")){
             sliceToStack(args[1], args[2]);
         }
         else if(args[0].equals("CropFromCoordonnate")){
             cropFromCoordinates(args[1]);
+        }
+        else if(args[0].equals("GenerateOverlay")){
+            genereOV(args[1]);
         }
         else{
             System.out.println("Argument le premier argument doit être   autocrop  ou   segmentation ou computeParameters");
