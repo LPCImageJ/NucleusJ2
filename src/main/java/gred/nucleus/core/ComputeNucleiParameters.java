ce25cc919a994caa798d7e518c06a3e8457c30c4
DesTristus <tristan.duos33@gmail.com>
2020-09-30 16:01:40 +0200

    add calibration constructor and correct warnings [skip ci]

--- src/main/java/gred/nucleus/core/ComputeNucleiParameters.java
+++ src/main/java/gred/nucleus/core/ComputeNucleiParameters.java
@@ -4,6 +4,7 @@ import gred.nucleus.FilesInputOutput.Directory;
 import gred.nucleus.FilesInputOutput.OutputTextFile;
 import gred.nucleus.plugins.PluginParameters;
 import ij.ImagePlus;
+import ij.measure.Calibration;
 import loci.plugins.BF;
 
 import java.io.File;
@@ -11,40 +12,50 @@ import java.util.ArrayList;
 
 public class ComputeNucleiParameters {
 
-    PluginParameters m_pluginParameters;
-    String m_rawImagesInputDirectory;
-    String m_segmentedImagesDirectory;
-
+    private  PluginParameters m_pluginParameters;
 
     /**
-     * Contructor
+     * Constructor with input, output and config files
      * @param rawImagesInputDirectory path to raw images
      * @param segmentedImagesDirectory path to segmented images associated
      * @param pathToConfig path to config file
      */
    public ComputeNucleiParameters(String rawImagesInputDirectory, String segmentedImagesDirectory, String pathToConfig){
        this.m_pluginParameters = new PluginParameters(rawImagesInputDirectory,segmentedImagesDirectory,pathToConfig);
-       this.m_rawImagesInputDirectory=rawImagesInputDirectory;
-       this.m_segmentedImagesDirectory=segmentedImagesDirectory;
+
 
     }
 
     /**
-     *
+     * Constructor with input and output files
      * @param rawImagesInputDirectory path to raw images
      * @param segmentedImagesDirectory path to segmented images associated
      */
     public ComputeNucleiParameters(String rawImagesInputDirectory, String segmentedImagesDirectory ){
         this.m_pluginParameters = new PluginParameters(rawImagesInputDirectory,segmentedImagesDirectory);
-        this.m_rawImagesInputDirectory=rawImagesInputDirectory;
-        this.m_segmentedImagesDirectory=segmentedImagesDirectory;
+
 
     }
 
     /**
-     * Compute nuclei parameters generate from segmentation (OTSU / GIFT)
+     * Constructor with input, output files and calibration from dialog.
+     * @param rawImagesInputDirectory path to raw images
+     * @param segmentedImagesDirectory path to segmented images associated
+     * @param cal calibration from dialog
+     */
+    public ComputeNucleiParameters(String rawImagesInputDirectory, String segmentedImagesDirectory ,
+                                   Calibration cal){
+        this.m_pluginParameters = new PluginParameters(rawImagesInputDirectory,segmentedImagesDirectory,
+        cal.pixelWidth,cal.pixelHeight,cal.pixelDepth);
+
+    }
+
+
+
+    /**
+     * Compute nuclei parameters generate from segmentation ( OTSU / GIFT)
      * Useful if parallel segmentation was use to get results parameter in the same folder.
-     * @throws Exception
+     * @throws Exception : file exceptions
      */
     public void run() throws Exception{
         Directory directoryRawInput = new Directory(this.m_pluginParameters.getInputFolder());
@@ -53,28 +64,32 @@ public class ComputeNucleiParameters {
         Directory directorySegmentedInput = new Directory(this.m_pluginParameters.getOutputFolder());
         directorySegmentedInput.listImageFiles(this.m_pluginParameters.getOutputFolder());
         directorySegmentedInput.checkIfEmpty();
-        ArrayList<File> rawImages =directoryRawInput.m_listeOfFiles;
         ArrayList<File> segmentedImages =directorySegmentedInput.m_listeOfFiles;
-        String outputCropGeneralInfoOTSU=this.m_pluginParameters.getAnalyseParameters()+getColnameResult();
-        for (short i = 0; i < segmentedImages.size(); ++i) {
-            File currentFile = segmentedImages.get(i);
-            ImagePlus Raw = new ImagePlus(this.m_pluginParameters.getInputFolder()+currentFile.separator+currentFile.getName());
-            ImagePlus[] Segmented = BF.openImagePlus(currentFile.getAbsolutePath());
+        StringBuilder outputCropGeneralInfoOTSU = new StringBuilder();
 
-            Measure3D mesure3D = new Measure3D(Segmented, Raw, this.m_pluginParameters.getXcalibration(Raw), this.m_pluginParameters.getYcalibration(Raw),this.m_pluginParameters.getZcalibration(Raw));
-            outputCropGeneralInfoOTSU+=mesure3D.nucleusParameter3D()+"\n";
+         outputCropGeneralInfoOTSU.append(this.m_pluginParameters.getAnalyseParameters()).append(getColNameResult());
+
+        for (File f : segmentedImages) {
+            ImagePlus Raw = new ImagePlus(this.m_pluginParameters.getInputFolder()+File.separator+f.getName());
+            ImagePlus[] Segmented = BF.openImagePlus(f.getAbsolutePath());
+
+            Measure3D measure3D = new Measure3D(Segmented, Raw, this.m_pluginParameters.getXcalibration(Raw), this.m_pluginParameters.getYcalibration(Raw),this.m_pluginParameters.getZcalibration(Raw));
+            outputCropGeneralInfoOTSU.append(measure3D.nucleusParameter3D()).append("\n");
+
         }
-        OutputTextFile resultFileOutputOTSU=new OutputTextFile(this.m_pluginParameters.getOutputFolder()
+        OutputTextFile resultFileOutputOTSU=new OutputTextFile(
+                this.m_pluginParameters.getOutputFolder()
                 +directoryRawInput.getSeparator()
                 +"result_Segmentation_Analyse.csv");
-        resultFileOutputOTSU.SaveTextFile( outputCropGeneralInfoOTSU);
+
+        resultFileOutputOTSU.SaveTextFile(outputCropGeneralInfoOTSU.toString());
 
     }
     /**
      *
      * @return columns names for results
      */
-    public String getColnameResult(){
+    private String getColNameResult(){
         return "NucleusFileName\t" +
                 "Volume\t" +
                 "Flatness\t" +
