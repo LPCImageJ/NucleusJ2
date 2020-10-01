bfddf03af24a11c232c61754d3e38f0274bb0326
DesTristus <tristan.duos33@gmail.com>
2020-10-01 17:12:34 +0200

    update parameters NJ2 [skip ci]

--- src/main/java/gred/nucleus/mains/computeSegmentationParameters.java
+++ src/main/java/gred/nucleus/mains/computeSegmentationParameters.java
@@ -84,11 +84,9 @@ public class computeSegmentationParameters {
                 "Volume\t" +
                 "Flatness\t" +
                 "Elongation\t" +
-                "Sphericity\t" +
                 "Esr\t" +
                 "SurfaceArea\t" +
-                "SurfaceAreaCorrected\t" +
-                "SphericityCorrected\t" +
+                "Sphericity\t" +
                 "MeanIntensityNucleus\t" +
                 "MeanIntensityBackground\t" +
                 "StandardDeviation\t" +
