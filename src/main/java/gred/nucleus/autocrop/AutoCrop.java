358d83607e9fb8b284512852ad91ea38351e7dd3
Pierre Pouchin <pierre.pouchin@uca.fr>
2021-02-16 15:56:51 +0000

    Update AutoCrop.java to save ROIs on OMERO

--- src/main/java/gred/nucleus/autocrop/AutoCrop.java
+++ src/main/java/gred/nucleus/autocrop/AutoCrop.java
@@ -604,6 +604,8 @@ public class AutoCrop {
 
 				ROIContainer roi = new ROIContainer(shapes);
 
+				image.saveROI(client, roi);
+
 				ImagePlus imgResu = image.toImagePlus(client, xBound, yBound, cBound, zBound, null);
 
 				Calibration cal = this.m_rawImg.getCalibration();
