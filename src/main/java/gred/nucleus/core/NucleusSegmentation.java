28101f8d776affd6fa4c8da640eeeb73707f257b
Tristan Dubos <tristan.duos33@gmail.com>
2021-05-04 13:53:42 +0200

    Test Gift  [skip ci]

--- src/main/java/gred/nucleus/core/NucleusSegmentation.java
+++ src/main/java/gred/nucleus/core/NucleusSegmentation.java
@@ -34,7 +34,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map.Entry;
 
-
 /**
  * Object segmentation method for 3D images. This segmentation used as initial threshold the method of Otsu, and then
  * select the object maximizing the sphericity of the segmented object.
--- src/main/java/gred/nucleus/mains/TestAutoCrop.java
+++ src/main/java/gred/nucleus/mains/TestAutoCrop.java
@@ -3,7 +3,13 @@ package gred.nucleus.mains;
 import gred.nucleus.autocrop.AutoCropCalling;
 import gred.nucleus.autocrop.AutocropParameters;
 import gred.nucleus.autocrop.CropFromCoordinates;
+import gred.nucleus.core.NucleusSegmentation;
+import ij.IJ;
+import ij.ImagePlus;
 
+import java.io.File;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.ArrayList;
 
 
@@ -63,20 +69,22 @@ public class TestAutoCrop {
 	 * @param args
 	 */
 	public static void main(String[] args) throws Exception {
-		
-		System.err.println("start program");
-		long maxMemory = Runtime.getRuntime().maxMemory();
-		/* Maximum amount of memory the JVM will attempt to use */
-		System.out.println("Maximum memory (bytes): " +
-		                   (maxMemory == Long.MAX_VALUE ? "no limit" : maxMemory * 1e-9));
-
-//		runAutoCropFolder("C:/Users/Martin/Documents/IMAGE_TEST_NJ/AUTOCROP/RAW_ND/","C:/Users/Martin/Documents/IMAGE_TEST_NJ/AUTOCROP/RESULTS/RAW_ND/");
-		runCropFromCoordinates("C:/Users/Martin/Documents/IMAGE_TEST_NJ/AUTOCROP/RESULTS/TIF_3D/tab_file.txt");
-		
-		System.err.println("The program ended normally.");
-		
-		System.out.println("Total memory (bytes): " +
-		                   Runtime.getRuntime().totalMemory() * 1e-9);
+		File currentFile      = new File("/media/titus/DATA/TEST_NJ2/TEST_GIFT/RAW/KAKU4-2--CRWN1-2--CRWN4-1_Cot_J13_STD_FIXE_H258_A1_0_C0.tif");
+		URLClassLoader child = new URLClassLoader(new URL[] {new URL("file:/home/titus/Lab/3D_Convex_Hull.jar")}, Main.class.getClassLoader());
+		ImagePlus img = new ImagePlus("/media/titus/DATA/TEST_NJ2/TEST_GIFT/RAW/KAKU4-2--CRWN1-2--CRWN4-1_Cot_J13_STD_FIXE_H258_A1_0_C0.tif");
+		IJ.run(img, "Make Convex Hull Vertices Stack...", "");
+		img.show();
+		/*
+		NucleusSegmentation nucleusSegmentation =
+				new NucleusSegmentation(currentFile, this.prefix, this.segmentationParameters);
+		nucleusSegmentation.preProcessImage();
+		nucleusSegmentation.findOTSUMaximisingSphericity();
+		nucleusSegmentation.checkBadCrop(this.segmentationParameters.inputFolder);
+		nucleusSegmentation.saveOTSUSegmented();
+		infoOtsu.append(nucleusSegmentation.getImageCropInfoOTSU());
+		nucleusSegmentation.saveGiftWrappingSeg();
+		infoGift.append(nucleusSegmentation.getImageCropInfoGIFT());
+		*/
 	}
 	
 }
